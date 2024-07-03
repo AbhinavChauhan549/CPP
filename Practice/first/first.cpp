19a404a43a9e3b4fa6cc947342f4441dee6077b3
Abhinav Chauhan <officialabhi549@gmail.com>
2024-07-03 15:38:15 +0530

    second commit

--- Practice/first/first.cpp
+++ Practice/first/first.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main()
 {
-  int arr[5] = {1, 5, 3, 9, 7};
+  // int arr[5] = {1, 5, 3, 9, 7};
   // sort(arr.begin(), arr.end());
   // cout << arr[3] << "";
 
-  int largest = INT_MIN, secondlargest = 0;
+  // int largest = INT_MIN, secondlargest = 0;
 
-  for (int i = 0; i < 5; i++)
-  {
-    if (arr[i] > largest)
-    {
-      largest = arr[i];
-    }
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   if (arr[i] > largest)
+  //   {
+  //     largest = arr[i];
+  //   }
 
-    if (secondlargest < arr[i] && secondlargest < largest)
-    {
-      secondlargest = arr[i];
-    }
-  }
-  cout << secondlargest << " ";
+  //   if (secondlargest < arr[i] && secondlargest < largest)
+  //   {
+  //     secondlargest = arr[i];
+  //   }
+  // }
+  cout << "secondlargest";
   return 0;
 }
