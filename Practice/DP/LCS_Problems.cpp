5d7df288b96fd7001ccad9984353f316184d4f85
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-08 13:28:13 +0530

    day 2

--- Practice/DP/LCS_Problems.cpp
+++ Practice/DP/LCS_Problems.cpp
@@ -193,6 +193,7 @@ int main()
 //==============================================================================================================================================================
 
 // 3 Shortest Common Supersequence
+/*
 #include <iostream>
 using namespace std;
 int SCS(string x, string y, int n, int m)
@@ -236,5 +237,57 @@ int main()
   int m = y.length();
 
   cout << SCS(x, y, n, m) << endl;
+  return 0;
+}
+  */
+
+//===============================================================================================================================================================
+
+// 4 Minimum number of insertions and deletions to convert string a to string b
+
+#include <iostream>
+using namespace std;
+
+int NoInsDel(string x, string y, int n, int m)
+{
+  int dp[n + 1][m + 1];
+
+  for (int i = 0; i < n + 1; i++)
+  {
+    for (int j = 0; j < m + 1; j++)
+    {
+      if (i == 0 || j == 0)
+      {
+        dp[i][j] = 0;
+      }
+    }
+  }
+
+  for (int i = 1; i < n + 1; i++)
+  {
+    for (int j = 1; j < m + 1; j++)
+    {
+      if (x[i - 1] == y[j - 1])
+      {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      }
+      else
+      {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  // number of deletions = length of string a - length of LCS
+  // number of insertions = length of string b - length of LCS
+  return m + n - (2 * dp[n][m]);
+}
+int main()
+{
+  string x = "heap";
+  string y = "pea";
+  int n = x.length();
+  int m = y.length();
+  cout << NoInsDel(x, y, n, m) << endl;
   return 0;
 }
