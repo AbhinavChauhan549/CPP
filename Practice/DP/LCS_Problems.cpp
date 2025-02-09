b9b1130be79484a6813efb95253d92281ceae486
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-09 11:03:32 +0530

    day 3

--- Practice/DP/LCS_Problems.cpp
+++ Practice/DP/LCS_Problems.cpp
@@ -412,7 +412,7 @@ int main()
 //==============================================================================================================================================
 
 // 7 Shortest Common Supersequence
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -522,5 +522,60 @@ int main()
   int m = y.length();
   // cout << PrintLCSRecursive(x, y, n, m) << endl;
   cout << PrintSCS(x, y, n, m) << endl;
+  return 0;
+}
+  */
+
+//==============================================================================================================================================
+
+// 8 Longest Repeating Subsequence
+
+#include <iostream>
+using namespace std;
+int LongestRepeatingSubsequence(string x, string y, int n, int m)
+{
+
+  int dp[n + 1][m + 1];
+
+  for (int i = 0; i < n + 1; i++)
+  {
+
+    for (int j = 0; j < m + 1; j++)
+    {
+
+      if (i == 0 || j == 0)
+      {
+        dp[i][j] = 0;
+      }
+    }
+  }
+
+  for (int i = 1; i < n + 1; i++)
+  {
+
+    for (int j = 1; j < m + 1; j++)
+    {
+
+      if (x[i - 1] == y[j - 1] && i != j)
+      {
+
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      }
+      else
+      {
+
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[n][m];
+}
+int main()
+{
+  string x = "aabebcdd";
+  string y = x;
+  int n = x.length();
+  int m = y.length();
+  cout << LongestRepeatingSubsequence(x, y, n, m) << endl;
   return 0;
 }
