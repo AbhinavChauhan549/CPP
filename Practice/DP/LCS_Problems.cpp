f7345d080936846e451680f2fb172f7c41ba46d6
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-09 11:20:03 +0530

    day 3

--- Practice/DP/LCS_Problems.cpp
+++ Practice/DP/LCS_Problems.cpp
@@ -529,7 +529,7 @@ int main()
 //==============================================================================================================================================
 
 // 8 Longest Repeating Subsequence
-
+/*
 #include <iostream>
 using namespace std;
 int LongestRepeatingSubsequence(string x, string y, int n, int m)
@@ -577,5 +577,63 @@ int main()
   int n = x.length();
   int m = y.length();
   cout << LongestRepeatingSubsequence(x, y, n, m) << endl;
+  return 0;
+}
+  */
+
+//==============================================================================================================================================
+
+// 9 Sequence Pattern Matching : is string a is a subsequence of string b
+
+#include <iostream>
+using namespace std;
+bool SequencePatternMatching(string x, string y, int n, int m)
+{
+
+  int dp[n + 1][m + 1];
+
+  for (int i = 0; i < n + 1; i++)
+  {
+
+    for (int j = 0; j < m + 1; j++)
+    {
+
+      if (i == 0 || j == 0)
+      {
+        dp[i][j] = 0;
+      }
+    }
+  }
+
+  for (int i = 1; i < n + 1; i++)
+  {
+
+    for (int j = 1; j < m + 1; j++)
+    {
+
+      if (x[i - 1] == y[j - 1])
+      {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      }
+      else
+      {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  if (dp[n][m] == n)
+  {
+    return true;
+  }
+  return false;
+}
+int main()
+{
+  string x = "AXY";
+  string y = "ADXCPY";
+  int n = x.length();
+  int m = y.length();
+
+  cout << SequencePatternMatching(x, y, n, m) << endl;
   return 0;
 }
