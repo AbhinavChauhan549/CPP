895328cec46d21cf64e484eb7b5154c7fd5128f8
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-10 20:47:09 +0530

    day 4

--- Practice/DP/MCM_Problems.cpp
+++ Practice/DP/MCM_Problems.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <climits>
+#include <cstring>
 using namespace std;
 
 bool isPalindrome(string s, int i, int j)
@@ -41,6 +42,39 @@ int PPRecursive(string s, int i, int j)
 }
 
 // Memorized Approach
+int PPMemorized(string s, int i, int j, int dp[100][100])
+{
+  if (i >= j)
+  {
+    return 0;
+  }
+  if (isPalindrome(s, i, j))
+  {
+    return 0;
+  }
+  if (dp[i][j] != -1)
+  {
+    return dp[i][j];
+  }
+  int min = INT_MAX;
+  for (int k = i; k < j; k++)
+  {
+    int temp = 1 + PPMemorized(s, i, k, dp) + PPMemorized(s, k + 1, j, dp);
+    if (temp < min)
+    {
+      min = temp;
+    }
+  }
+  dp[i][j] = min;
+  return dp[i][j];
+}
+
+int PPMemorized(string s, int i, int j)
+{
+  int dp[100][100];
+  memset(dp, -1, sizeof(dp));
+  return PPMemorized(s, i, j, dp);
+}
 
 int main()
 {
@@ -49,5 +83,7 @@ int main()
 
   int n = s.length();
   cout << PPRecursive(s, 0, n - 1) << endl;
+  cout << PPMemorized(s, 0, n - 1) << endl;
+
   return 0;
 }
