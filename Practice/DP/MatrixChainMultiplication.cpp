7b47329fab744351f4210a0c64d43ebd2e593a4a
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-10 20:11:16 +0530

    day 4

---
+++ Practice/DP/MatrixChainMultiplication.cpp
@@ -0,0 +1,39 @@
+// MCM Problems : we are given a sequence of matrices of different dimensions to be multiplied, we need to find the most efficient way or cost to multiply these matrices.
+
+// 1. Matrix Chain Multiplication
+// 2. Print MCM
+// 3. Evaluate Expression to True / Boolean Parenthesization
+// 4. Scrambled String
+// 5. Egg Dropping Problem
+// 6. Palindrome Partitioning
+// 7. Min and Max Values of an Expression
+
+#include <iostream>
+#include <climits>
+using namespace std;
+int MatrixChainMultiplication(int p[], int i, int j)
+{
+
+  if (i == j)
+  {
+    return 0;
+  }
+
+  int min = INT_MAX;
+
+  for (int k = i; k < j; k++)
+  {
+    int temp = MatrixChainMultiplication(p, i, k) + MatrixChainMultiplication(p, k + 1, j) + p[i - 1] * p[k] * p[j];
+    if (temp < min)
+    {
+      min = temp;
+    }
+  }
+  return min;
+}
+int main()
+{
+  int arr[] = {40, 20, 30, 10, 30};
+  cout << MatrixChainMultiplication(arr, 1, 4) << endl;
+  return 0;
+}
