02a0cf5a8e064ccdd2e710364a925e6a5a755c0b
Abhinav Chauhan <officialabhi549@gmail.com>
2025-02-11 18:40:19 +0530

    day 5

--- Practice/DP/MCM_Problems.cpp
+++ Practice/DP/MCM_Problems.cpp
@@ -69,11 +69,59 @@ int PPMemorized(string s, int i, int j, int dp[100][100])
   return dp[i][j];
 }
 
+// Optimized Memorized Approach
+int PPMemorizedO(string s, int i, int j, int dp[100][100])
+{
+  if (i >= j)
+  {
+    return 0;
+  }
+  if (isPalindrome(s, i, j))
+  {
+    return 0;
+  }
+  if (dp[i][j] != -1)
+  {
+    return dp[i][j];
+  }
+  int min = INT_MAX;
+  for (int k = i; k < j; k++)
+  {
+    int left = 0, right = 0;
+    if (dp[i][k] != -1)
+    {
+      left = dp[i][k];
+    }
+    else
+    {
+      left = PPMemorizedO(s, i, k, dp);
+      dp[i][k] = left;
+    }
+    if (dp[k + 1][j] != -1)
+    {
+      right = dp[k + 1][j];
+    }
+    else
+    {
+      right = PPMemorizedO(s, k + 1, j, dp);
+      dp[k + 1][j] = right;
+    }
+    // int temp = 1 + PPMemorized(s, i, k, dp) + PPMemorized(s, k + 1, j, dp);
+    int temp = 1 + left + right;
+    if (temp < min)
+    {
+      min = temp;
+    }
+  }
+  dp[i][j] = min;
+  return dp[i][j];
+}
+
 int PPMemorized(string s, int i, int j)
 {
   int dp[100][100];
   memset(dp, -1, sizeof(dp));
-  return PPMemorized(s, i, j, dp);
+  return PPMemorizedO(s, i, j, dp);
 }
 
 int main()
