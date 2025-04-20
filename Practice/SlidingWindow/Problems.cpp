e998dcf9666586dcba269bd678cb442df516b0e0
Abhinav Chauhan <officialabhi549@gmail.com>
2025-04-20 18:03:36 +0530

    4th problem SW

--- Practice/SlidingWindow/Problems.cpp
+++ Practice/SlidingWindow/Problems.cpp
@@ -86,4 +86,27 @@ int characterReplacement(string s, int k) {
 
 //=================================================================================================================================================
 
-//
+// Binary Subarrays with Sum / count of subarrays with sum k
+
+/*
+int f(vector<int>& nums, int goal) {
+  int n = nums.size();
+  if (goal < 0) return 0;  // Edge case for negative goal
+  int l = 0, r = 0, count = 0, currSum = 0;
+
+  while (r < n) {
+      currSum += nums[r];
+      while (currSum > goal) {
+          currSum -= nums[l];
+          l++;
+      }
+      count += (r - l + 1);  // Number of valid subarrays
+      r++;
+  }
+  return count;
+}
+
+int numSubarraysWithSum(vector<int>& nums, int goal) {
+  return f(nums, goal) - f(nums, goal - 1);
+}
+  */
