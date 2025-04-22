79502ed7576be4c755d30f82138c26b018921527
Abhinav Chauhan <officialabhi549@gmail.com>
2025-04-22 11:25:50 +0530

    Shortest job first solution

--- Practice/Greedy/Problems.cpp
+++ Practice/Greedy/Problems.cpp
@@ -21,3 +21,21 @@ int findContentChildren(vector<int>& g, vector<int>& s) {
   return r;
 }
   */
+
+//==========================================================================================================================================
+
+// Shortest Job First
+
+/*
+long long solve(vector<int>& bt) {
+  sort(bt.begin(),bt.end());
+
+   int t=0 , wttime=0;
+  for(int i =0 ; i<bt.size() ;i++){
+   wttime +=t;
+   t += bt[i];
+  }
+
+  return (wttime/bt.size());
+}
+  */
