68b9f896efc9922d9668f4aca4973e6b07fc77e7
Abhinav Chauhan <officialabhi549@gmail.com>
2025-04-23 10:01:13 +0530

    Jump game I

--- Practice/Greedy/Problems.cpp
+++ Practice/Greedy/Problems.cpp
@@ -83,3 +83,28 @@ class Solution {
   };
 
   */
+//==============================================================================================================================================
+
+// JUmp Game : You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+
+/*bool canJump(vector<int>& nums) {
+
+    int  n= nums.size();
+
+    int maxIndexThatCanBeReachedFromThatElement =0 ;
+
+    for( int i =0 ; i<n ; i++){
+
+        if(i > maxIndexThatCanBeReachedFromThatElement){
+            return false;
+        }
+
+        maxIndexThatCanBeReachedFromThatElement= max(maxIndexThatCanBeReachedFromThatElement, i+nums[i]);
+    }
+
+    return true;
+}
+
+*/
+//============================================================================================================================================
